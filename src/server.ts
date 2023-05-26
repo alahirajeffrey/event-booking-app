9ebabe620d3127c2e0254e19e74c1c58b1fb5afb
alahirajeffrey <alahirajeffrey@gmail.com>
2023-05-26 23:25:56 +0100

    setup event routes on server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import authRouter from "./routes/auth.route";
+import eventRouter from "./routes/event.route";
 
 const server: Express = express();
 
 server.use(express.json());
 
 // setup routes here
-server.use("/api/v1/", authRouter);
+server.use("/api/v1/auth/", authRouter);
+server.use("/api/v1/event/", eventRouter);
 
 server.get("/api/v1/", (req: Request, res: Response) => {
   return res.status(200).json({ message: "welcome" });
