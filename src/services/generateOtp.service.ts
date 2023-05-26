5c4f554ac14af50c19d9cf66c14ef0c253ae07fc
alahirajeffrey <alahirajeffrey@gmail.com>
2023-05-26 22:20:11 +0100

    renamed files

--- src/services/generateOtp.service.ts
+++
@@ -1,7 +0,0 @@
-/**
- * generates a one time password for user
- * @returns : string containing one time password
- */
-export const generateOtp = (): string => {
-  return Math.floor(Math.random() * 899999 + 10000).toString();
-};
