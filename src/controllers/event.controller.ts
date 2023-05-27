d8f34d63cfbaeee5330028b686691278c81cb3cb
alahirajeffrey <alahirajeffrey@gmail.com>
2023-05-27 08:38:02 +0100

    added controller to add and update seat prices

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -12,6 +12,7 @@ import decodeToken from "../helpers/decodeToken.helper";
 const checkIfEventExists = async (eventId: string) => {
   return await prisma.event.findFirst({ where: { id: eventId } });
 };
+
 /**
  * create an event
  * @param req : request object containing title, description, date, location, organizerId and price
@@ -84,32 +85,6 @@ export const getEventById = async (
     const event = await prisma.event.findFirst({ where: { id: eventId } });
 
     return res.status(StatusCodes.OK).json(event);
-  } catch (error: any) {
-    return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ message: error.message });
-  }
-};
-
-export const getAllEventsInADay = async (
-  req: Request,
-  res: Response
-): Promise<Response<ApiResponse>> => {
-  try {
-    return res.status(StatusCodes.OK).json({ message: "event" });
-  } catch (error: any) {
-    return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ message: error.message });
-  }
-};
-
-export const getAllEventsInAMonth = async (
-  req: Request,
-  res: Response
-): Promise<Response<ApiResponse>> => {
-  try {
-    return res.status(StatusCodes.OK).json({ message: "event" });
   } catch (error: any) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
@@ -200,6 +175,76 @@ export const deleteEvent = async (
     await prisma.event.delete({ where: { id: eventId } });
 
     return res.status(StatusCodes.OK).json({ message: "event deleted" });
+  } catch (error: any) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
+};
+
+/**
+ *
+ * @param req : request object containing eventId and organizerId params
+ * @param res : response object
+ * @returns : status code and response message
+ */
+export const setOrUpdateEventPrice = async (
+  req: Request,
+  res: Response
+): Promise<Response<ApiResponse>> => {
+  try {
+    const { eventId, organizerId } = req.params;
+    const seatPrice = req.body.seatPrice;
+
+    // check if event exists
+    const eventExists = await checkIfEventExists(eventId);
+    if (!eventExists) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "event does not exist" });
+    }
+
+    // get userId from jwt and ensure only owner can delete an event
+    const authHeader = req.headers["authorization"] || "defaultAuthHeader";
+    const user: any = decodeToken(authHeader);
+
+    if (organizerId != user.id) {
+      return res
+        .status(StatusCodes.METHOD_NOT_ALLOWED)
+        .json({ message: "you cannot delete an event you did not create" });
+    }
+
+    await prisma.event.update({
+      where: { id: eventId },
+      data: { seatPrice: seatPrice },
+    });
+    return res.status(StatusCodes.OK).json({ message: "price added" });
+  } catch (error: any) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
+};
+
+export const getAllEventsInADay = async (
+  req: Request,
+  res: Response
+): Promise<Response<ApiResponse>> => {
+  try {
+    return res.status(StatusCodes.OK).json({ message: "event" });
+  } catch (error: any) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
+};
+
+export const getAllEventsInAMonth = async (
+  req: Request,
+  res: Response
+): Promise<Response<ApiResponse>> => {
+  try {
+    return res.status(StatusCodes.OK).json({ message: "event" });
   } catch (error: any) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
