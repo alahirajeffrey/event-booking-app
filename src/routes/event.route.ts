37d70f16f1d8f81ea09d2583a520ea047697c104
alahirajeffrey <alahirajeffrey@gmail.com>
2023-05-29 20:44:22 +0100

    updated routes

--- src/routes/event.route.ts
+++ src/routes/event.route.ts
@@ -10,56 +10,56 @@ const eventRouter = Router();
 eventRouter.post(
   "/",
   passport.authenticate("jwt", { session: false }),
-  validate(validations.validateCreateEvent),
+  validate(validations.validateCreateEvent, {}, {}),
   eventControllers.createEvent
 );
 
 eventRouter.get(
   "/organizer/:organizerId",
   passport.authenticate("jwt", { session: false }),
-  validate(validations.validateGetEventByOrganizer),
+  validate(validations.validateGetEventByOrganizer, {}, {}),
   eventControllers.getEventsByOrganizerId
 );
 
 eventRouter.get(
   "/:eventId",
   passport.authenticate("jwt", { session: false }),
-  validate(validations.validateGetEventById),
+  validate(validations.validateGetEventById, {}, {}),
   eventControllers.getEventById
 );
 
 eventRouter.get(
   "/:day",
   //   passport.authenticate("jwt"),
-  validate(validations.validateGetAllEventsInADay),
+  validate(validations.validateGetAllEventsInADay, {}, {}),
   eventControllers.getAllEventsInADay
 );
 
 eventRouter.get(
   "/:month",
   //   passport.authenticate("jwt"),
-  validate(validations.validateGetAllEventsInAMonth),
+  validate(validations.validateGetAllEventsInAMonth, {}, {}),
   eventControllers.getAllEventsInAMonth
 );
 
 eventRouter.patch(
   "/:eventId/:organizerId",
   passport.authenticate("jwt", { session: false }),
-  validate(validations.validateUpdateEvent),
+  validate(validations.validateUpdateEvent, {}, {}),
   eventControllers.updateEvent
 );
 
 eventRouter.delete(
   "/:eventId/:organizerId",
   passport.authenticate("jwt", { session: false }),
-  validate(validations.validateDeleteEvent),
+  validate(validations.validateDeleteEvent, {}, {}),
   eventControllers.deleteEvent
 );
 
 eventRouter.patch(
   "/price/:eventId/:organizerId",
   passport.authenticate("jwt", { session: false }),
-  validate(validations.validateSetOrUpdateEventPrice),
+  validate(validations.validateSetOrUpdateEventPrice, {}, {}),
   eventControllers.setOrUpdateEventPrice
 );
 
