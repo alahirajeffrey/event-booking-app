c1989d213a681fe6c9352f4c2c6700fb83282992
alahirajeffrey <alahirajeffrey@gmail.com>
2023-05-30 15:42:52 +0100

    function to send email using elastic email api

--- src/helpers/sendEmail.helper.ts
+++ src/helpers/sendEmail.helper.ts
@@ -1,7 +1,32 @@
 import axios from "axios";
+import logger from "./logger";
 
-const sendEmail = (
+const EMAIL_API_KEY = process.env.elasticEmailApiKey;
+const EMAIL_BASE_URL = process.env.elasticEmailBaseUrl;
+
+/**
+ * function to send emails using the elastic email api
+ * @param recieverEmail : email of the reciever
+ * @param emailTitle : title of the email
+ * @param emailContent : content of email
+ * @param emailFrom : email sender
+ */
+const sendEmail = async (
   recieverEmail: string,
   emailTitle: string,
-  emailContent: string
-) => {};
+  emailContent: string,
+  emailFrom: string
+) => {
+  try {
+    const response = await axios.post(
+      `${EMAIL_BASE_URL}/send?apikey=${EMAIL_API_KEY}&from=${emailFrom}&to=${recieverEmail}&subject=${emailTitle}&bodyText=${emailContent}`
+    );
+    logger.info(response.status);
+    return response;
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+};
+
+export default sendEmail;
