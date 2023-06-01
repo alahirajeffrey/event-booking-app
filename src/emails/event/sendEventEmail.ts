03b36ff9c2ec5fd7c46dc6b11280420e314f5e90
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-01 04:22:51 +0100

    added fn to send price changes and updated doc

--- src/emails/event/sendEventEmail.ts
+++ src/emails/event/sendEventEmail.ts
@@ -1,8 +1,7 @@
-import { error } from "console";
 import logger from "../../helpers/logger";
 import transporter from "../transporter";
 
-enum EventStatusEnum {
+export enum EventStatusEnum {
   Created = "created",
   Updated = "updated",
   Deleted = "deleted",
@@ -11,7 +10,10 @@ enum EventStatusEnum {
 /**
  * function to send otp email
  * @param to : email of the reciepient
- * @param otp : otp to be sent
+ * @param eventId : id of the event
+ * @param time : date and time of the event
+ * @param location : location of the  event
+ * @param eventStatus : status of the event i.e created, updated, deleted
  */
 const sendEventEmail = async (
   to: string,
@@ -40,7 +42,36 @@ const sendEventEmail = async (
     })
     .catch((err) => {
       logger.error(err);
-      throw error;
+      throw err;
+    });
+};
+
+/**
+ * function to send otp email
+ * @param to : email of the reciepient
+ * @param eventId : id of the event
+ */
+export const sendPriceSetorUpdateEmail = async (
+  to: string,
+  eventId: string
+) => {
+  const mailOptions = {
+    from: "alahirajeffrey@gmail.com",
+    to: to,
+    subject: "Event created",
+    text: `Hi there, 
+    
+    The seat price for your event with id: ${eventId} has been updated.`,
+  };
+
+  await transporter
+    .sendMail(mailOptions)
+    .then((response) => {
+      logger.info(`Email sent: ${response.response}`);
+    })
+    .catch((err) => {
+      logger.error(err);
+      throw err;
     });
 };
 
