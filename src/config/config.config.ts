53acb71d3b8ae0c3da2c2da6cdc3af7c116b4bd4
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-27 17:41:21 +0100

    added rabbitmq url env to config file

--- src/config/config.config.ts
+++ src/config/config.config.ts
@@ -7,6 +7,13 @@ const config = {
   NODE_ENV: process.env.NODE_ENV || "development",
 
   PORT: process.env.PORT || 5000,
+
+  OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID,
+  OAUTH_CLIENT_SECRET: process.env.OAUTH_CLIENT_SECRET,
+  OAUTH_REFRESH_TOKEN: process.env.OAUTH_REFRESH_TOKEN,
+  OAUTH_REDIRECT_BASE_URI: process.env.OAUTH_REDIRECT_BASE_URI,
+
+  RABBITMQ_URL: process.env.RABBITMQ_URL,
 };
 
 export default config;
