c742995576149d7d9970ad3baa415be3de7b4f06
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-28 18:59:35 +0100

    renamed variables

--- src/config/config.config.ts
+++ src/config/config.config.ts
@@ -2,8 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  JWT_SECRET: process.env.JWT_SECRET || "secret",
-  EXPIRES_IN: process.env.EXPIRES_IN || "30m",
+  JWT_ACCESS_TOKEN: process.env.JWT_ACCESS_TOKEN || "access_secret",
+  JWT_REFRESH_TOKEN: process.env.JWT_ACCESS_TOKEN || "refresh_secret",
+  EXPIRES_IN: process.env.EXPIRES_IN || "1d",
+
   NODE_ENV: process.env.NODE_ENV || "development",
 
   PORT: process.env.PORT || 5000,
