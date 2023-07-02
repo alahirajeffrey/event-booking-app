e98fc5352f411c8c017aa3cd430f11f8b1c60799
alahirajeffrey <alahirajeffrey@gmail.com>
2023-07-02 10:41:50 +0100

    setting up google oauth

--- src/config/passport.config.ts
+++ src/config/passport.config.ts
@@ -2,6 +2,24 @@ import prisma from "./prisma.config";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import passport from "passport";
 import config from "../config/config.config";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+// Configure Google OAuth2 strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: "your-client-id",
+      clientSecret: "your-client-secret",
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Add your logic to handle the user's profile data and authentication
+      // You can store the user in your database or perform any necessary actions
+      console.log(profile);
+      done(null, profile);
+    }
+  )
+);
 
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
