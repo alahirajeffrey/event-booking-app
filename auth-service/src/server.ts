30f20eca4f40bfa1cd1dd8c9dd7766a8cc2dbf4b
alahirajeffrey <alahirajeffrey@gmail.com>
2023-08-31 10:28:34 +0100

    setup server

---
+++ auth-service/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from "express";
+import authRouter from "./routes/auth.route";
+import limiter from "./middlewares/rateLimiter.middleware";
+
+const server: Express = express();
+
+server.use(express.json());
+server.use(limiter);
+
+// setup routes here
+server.use("/api/v1/auth", authRouter);
+
+server.get("/api/v1/", (req: Request, res: Response) => {
+  return res.status(200).json({ message: "welcome" });
+});
+
+export default server;
